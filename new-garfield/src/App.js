caf6e0d4b3f9385608fd95ae401c6ce46b7b9218
PotatoTacohybrid <43556550+PotatoTacohybrid@users.noreply.github.com>
2019-02-18 15:51:33 -0600

    Changed Idea

--- new-garfield/src/App.js
+++ new-garfield/src/App.js
@@ -10,128 +10,56 @@ class App extends Component {
     super(props)
 
     this.state = {
-      inputDisabled: true,
-      input: '',
-      inputPlaceholder: '',
-      messages: []
+      data: [],
     }
 
-    this.timer = this.timer.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.getJoke = this.getJoke.bind(this)
   }
 
-  timer() {
-    let d = new Date()
+  getJoke() {
+    const apiUrl = 'https://icanhazdadjoke.com';
 
-    let curTime = d.getTime()
+    const request = new XMLHttpRequest();
 
-    let oldTime = localStorage.getItem('oldTime')
+    request.open('GET', apiUrl)
 
-    if (!oldTime) { return true; }
+    request.setRequestHeader('Accept', 'application/json')
 
+    request.onload = () => {
+      const response = JSON.parse(request.responseText);
 
-
-
-    setTimeout(function() {
-      let timeLeft = Math.round((oldTime / 1000) - (curTime / 1000) + 59)
-
-
-        if (timeLeft <= 0) {
-          this.setState({
-            inputDisabled: false,
-            input: '',
-            inputPlaceholder: 'Your Garfield comment'
-        })
-
-
-        localStorage.removeItem('oldTime');
-
-        return true;
-      };
 
       this.setState({
-        inputPlaceholder: (timeLeft >= 10) ? `Time until next message 0:${timeLeft}`: `Time until next message 0:0${timeLeft}`
+        data: [...this.state.data, response.joke],
       })
-    }.bind(this), 1000)
-  
-  };
 
+    }
 
-
-  handleChange(event) {
-    this.setState({
-      input: event.target.value
-    })
-  };
-
-  handleSubmit() {
-    this.setState({
-      messages: [...this.state.messages, this.state.input],
-      input: '',
-    })
-
-    let d = new Date()
-
-    localStorage.setItem('oldTime', d.getTime())
-    this.timer()
+    request.send()
   }
-
 
   render() {
-
-
     return (
-      <div className="App">
-        <textarea onChange={this.handleChange} value={this.state.input} placeholder={this.state.inputPlaceholder}></textarea>
-        <button disabled={!this.timer()} onClick={this.handleSubmit}>Submit</button>
-        <p>{this.state.messages}</p>
+      <div style={{textAlign: 'center'}}>
+        <button onClick={this.getJoke}>Get new joke</button>
+        <br />
+        <ul>
+          <h1>
+            {this.state.data.map((joke, index) => {
+                return (
+                  <li key={index}>{joke}</li>
+                )
+              })
+            }
+          </h1>
+      </ul>
+        
       </div>
-    );
+    )
   }
+  
 }
 
 export default App;
 
 
-//function timer() {
-
-//  let d = new Date()
-
-//  let curTime = d.getTime()
-
-//  let oldTime = localStorage.getItem('oldTime')
-
-
-//  if (!localStorage.getItem('oldTime')) { return true; }
-
-//  newText.disabled = true;
-//  button.disabled = true;
-
-
-//  setTimeout(function() {
-
-
-//      let timeLeft = Math.round((oldTime / 1000) - (curTime / 1000) + 59)
-
-
-//      if (timeLeft <= 0) {
-//
-//          newText.placeholder = `Your Garfield comment`;
-
-//          button.disabled = false;
-//          newText.disabled = false;
-
-
-//          localStorage.removeItem('oldTime');
-
-
-//          return true;
-//     }
-//     newText.placeholder = (timeLeft >= 10) ? `Time until next message 0:${timeLeft}`: `Time until next message 0:0${timeLeft}`;
-
-///      timer();
-
-//  }, 1000)
-
-//}
